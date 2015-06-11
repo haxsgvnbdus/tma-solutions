272ebb4070d67319fd6b5734ad0b0f3e6a90a589
hanngo2911 <han.ngo@tcu.edu>
2015-06-11 08:57:19 -0700

    DetailProductSpec and IndexProductSpec files tracked

---
+++ PhoneStore.CMS/ViewModels/DetailProductSpec.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PhoneStore.Model.Catalog;
+using PhoneStore.Model.Media;
+using PhoneStore.Model.Discounts;
+using PhoneStore.Model.Catalog;
+
+
+namespace PhoneStore.CMS.ViewModels
+{
+    public class DetailProductSpec
+    {
+        //public int ID { get; set; }
+        public IndexProductSpec IndexSpec { get; set; }
+
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+        //public IEnumerable<ProductManufacturer> ProductManufacturers { get; set; }
+        //public IEnumerable<ProductPicture> ProductPictures { get; set; }
+        //public IEnumerable<ProductComment> ProductComments { get; set; }
+        //public IEnumerable<ProductTag> ProductTags { get; set; }
+        //public IEnumerable<Discount> Discounts { get; set; 
+        
+        
+    }
+}
---
+++ PhoneStore.CMS/ViewModels/IndexProductSpec.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneStore.CMS.ViewModels
+{
+    public class IndexProductSpec
+    {
+  
+        public int ID { get; set; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public decimal Price { get; set; }
+        public bool IsHot { get; set; }
+        public int StockQuantity { get; set; }
+        public string MetaKeywords { get; set; }
+        public string MetaDescription { get; set; }
+
+        public string MetaTitle { get; set; }
+
+        public string Sku { get; set; }
+        public int ProductTypeId { get; set; }
+
+        public DateTime CreatedOnUtc { get; set; }
+
+        public DateTime UpdatedOnUtc { get; set; }
+
+
+    }
+}
